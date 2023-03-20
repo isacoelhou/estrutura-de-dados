66df0388d300ae92ad0fd0837067afcabfefde36
isadora.araujo1@unioeste.br <isadora.araujo1@unioeste.br>
2023-03-20 09:02:57 -0300

    att

--- go/1.go
+++ go/1.go
@@ -9,8 +9,8 @@ import "fmt"   // Importação de bibliotecas. Equivale ao "#include < >" em C++
 func main() {  // Função principal. A indentação neste formato é obrigatória e 
                // deve ser seguida.
    //Comandos 
-   fmt.Print("Bem Vindos ao GO!\n", 123)
-   fmt.Prinln("Bem Vindos ao GO!\n", 123)
-   fmt.Prinln("Bem Vindos ao GO! %d\n", 123)
+   fmt.Print("Bem Vindos ao GO!", 123)
+   fmt.Println("\nBem Vindos ao GO!", 123)
+   fmt.Printf("Bem Vindos ao GO! %d\n", 123)
 
 }
